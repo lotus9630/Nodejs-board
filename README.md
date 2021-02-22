374e1b3728a81cd561e9c98b4b3f3c6891efb79d
lotus <lotus9630@gmail.com>
2021-02-22 22:20:09 +0900

    Make READMEmd

--- README.md
+++ README.md
@@ -1,32 +1,54 @@
-# Restful api 게시판 
-사용 스택 Nodejs, MySQL 
+# Nodejs기반 게시판
 
-#기능 
+<img src="./captureImg/메인페이지.PNG"></img>
+HTML, CSS, Javascript, Nodejs, MySQL
 
-로그인, 회원가입
-글 쓰기, 글 수정, 글 삭제
+## 기능
 
+- ### 로그인
 
-#DB - MySQL 
+  <img src="./captureImg/로그인.PNG"></img>
 
-create database free_board; // DB 생성 
+- ### 회원가입
 
-CREATE TABLE `user_list` ( // 유저 테이블 생성 
+  <img src="./captureImg/회원가입.PNG"></img>
+
+- ### 글 읽기
+
+  <img src="./captureImg/글 읽기.PNG"></img>
+
+- ### 글 쓰기
+
+  <img src="./captureImg/글 쓰기.PNG"></img>
+
+- ### 글 수정
+
+  <img src="./captureImg/글 수정.PNG"></img>
+
+- ### 글 삭제
+  글 삭제 버튼을 눌러 "Title 3" 글을 삭제한 모습
+  <img src="./captureImg/글 삭제.PNG"></img>
+
+## 데이터 베이스 테이블 구조
+
+### DB 생성
+
+    create database nodejs_board;
+
+### 유저 테이블 생성
+
+    CREATE TABLE `user_list` (
     `id` char(20) NOT NULL,
     `pw` text(200) NOT NULL,
     `salt` text(200) NOT NULL,
     `nickname` char(20) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO user_list(id, pw, salt, nickname)  // 회원가입 
-VALUES ("sample", "sample", "sample","sample");
+### 글 목록 테이블 생성
 
-CREATE TABLE `writing_list` ( // 글 목록 테이블 생성 
+    CREATE TABLE `writing_list` (
     `title` char(20) NOT NULL,
     `description` text(200) NOT NULL,
     `writer` char(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-INSERT INTO writing_list(title, description, writer) // 글 쓰기 
-VALUES ("sample", "sample", "sample");
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- app.js
+++ app.js
@@ -1,57 +1,63 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const session = require('express-session');
-const fs = require("fs")
-const bodyParser = require('body-parser');
-const crypto = require('crypto');
-const mysql = require('mysql');
-const MySQLStore = require('express-mysql-session')(session);    
-const template = require('./views/template.js')
-const methodOverride = require('method-override')
+const express = require("express");
+const app = express();
+const port = 3000;
+const session = require("express-session");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const crypto = require("crypto");
+const mysql = require("mysql");
+const MySQLStore = require("express-mysql-session")(session);
+const template = require("./views/template.js");
+const methodOverride = require("method-override");
 
-
-const options ={
-    host: 'localhost',
-    port: 3306,
-    user     : 'root',
-    password : '111111',
-    database : 'free_board'
+const options = {
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "111111",
+  database: "nodejs_board",
 };
 
-const sessionStore = new MySQLStore(options);                   
+const sessionStore = new MySQLStore(options);
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '111111',
-  database : 'free_board'
+  host: "localhost",
+  user: "root",
+  password: "111111",
+  database: "nodejs_board",
 });
 
-app.use(session({                                              
-  secret:"asdfasffdas",
-  resave:false,
-  saveUninitialized:true,
-  store: sessionStore                                         
-}))
+app.use(
+  session({
+    secret: "asdfasffdas",
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+  })
+);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css")); // redirect CSS bootstrap
+app.use(methodOverride("_method"));
 
-app.get('/', (req, res) => {
-    let title_list="<ul>"
-    connection.query(`select title from writing_list`, function (error, results, fields) {
-        if (error) {
-            throw(error)
-        } else {
-            results.forEach(element => {
-                title_list = title_list + `<li><a href="/page/${element.title}">${element.title}</a></li>`
-            });
-            title_list = title_list + "</ul>"
-            if (req.session.logined == true) {
-                res.send(template.body(
-                    `<li class="nav-item active">
+app.get("/", (req, res) => {
+  let title_list = "<ul>";
+  connection.query(
+    `select title from writing_list`,
+    function (error, results, fields) {
+      if (error) {
+        throw error;
+      } else {
+        results.forEach((element) => {
+          title_list =
+            title_list +
+            `<li><a href="/page/${element.title}">${element.title}</a></li>`;
+        });
+        title_list = title_list + "</ul>";
+        if (req.session.logined == true) {
+          res.send(
+            template.body(
+              `<li class="nav-item active">
                     <a class="nav-link" href="/">Home
                         <span class="sr-only">(current)</span>
                     </a>
@@ -62,13 +68,14 @@ app.get('/', (req, res) => {
                     <li class="nav-item">
                     <a class="nav-link" href="/logout">Logout</a>
                     </li>`,
-                    `<a href="/create"><button type="button" class="btn btn-success">글 쓰기</button></a>`,  
-                    `<h1>${title_list}</h1>`
-                ))
-        
-            } else {
-                res.send(template.body(
-                    `<li class="nav-item active">
+              `<a href="/create"><button type="button" class="btn btn-success">글 쓰기</button></a>`,
+              `<h1>${title_list}</h1>`
+            )
+          );
+        } else {
+          res.send(
+            template.body(
+              `<li class="nav-item active">
                     <a class="nav-link" href="/">Home
                         <span class="sr-only">(current)</span>
                     </a>
@@ -79,20 +86,24 @@ app.get('/', (req, res) => {
                     <li class="nav-item">
                     <a class="nav-link" href="/signup">Signup</a>
                     </li>`,
-                    ``, // 비로그인시 button 비활성화
-                    `<h1>${title_list}</h1>`
-                ))
-            }
-        };
-    });
-})
+              ``, // 비로그인시 button 비활성화
+              `<h1>${title_list}</h1>`
+            )
+          );
+        }
+      }
+    }
+  );
+});
 
-app.get('/create',(req, res) => { // 글 쓰기 UI 요청
-    if (req.session.logined == false){
-        res.redirect('/')
-    } else {
-        res.send(template.body(
-            `<li class="nav-item active">
+app.get("/create", (req, res) => {
+  // 글 쓰기 UI 요청
+  if (req.session.logined == false) {
+    res.redirect("/");
+  } else {
+    res.send(
+      template.body(
+        `<li class="nav-item active">
             <a class="nav-link" href="/">Home
                 <span class="sr-only">(current)</span>
             </a>
@@ -103,8 +114,8 @@ app.get('/create',(req, res) => { // 글 쓰기 UI 요청
             <li class="nav-item">
             <a class="nav-link" href="/logout">Logout</a>
             </li>`,
-            ``,  
-            `<form action="/create" method="POST">
+        ``,
+        `<form action="/create" method="POST">
                 <p>
                     <input name="title" type="text"  placeholder="제목">
                 </p>
@@ -119,26 +130,34 @@ app.get('/create',(req, res) => { // 글 쓰기 UI 요청
              </form>
             
             `
-        ))
+      )
+    );
+  }
+});
+
+app.post("/create", (req, res) => {
+  // 글 생성
+  console.log(req.body);
+  console.log(req.session.nickname);
+  connection.query(
+    `INSERT INTO writing_list(title, description, writer) 
+    VALUES ("${req.body.title}", "${req.body.description}", "${req.session.nickname}");`,
+    function (error, results, fields) {
+      console.log(results);
+      if (error) {
+        throw error;
+      } else {
+        res.redirect("/");
+      }
     }
-    
-})
+  );
+});
 
-app.post('/create',(req,res)=>{ // 글 생성 
-    connection.query(`INSERT INTO writing_list(title, description, writer) 
-    VALUES ("${req.body.title}", "${req.body.description}", "${req.session.nickname}");`, function (error, results, fields) {
-        if (error) {
-            throw(error)
-        } else {
-            connection.end();
-            res.redirect("/")
-        };
-    });
-})
-
-app.get('/page/:title', (req, res) => {
-    connection.query(`select description from writing_list where title="${req.params.title}" `, function (error, results, fields) {
-        res.send(`
+app.get("/page/:title", (req, res) => {
+  connection.query(
+    `select description from writing_list where title="${req.params.title}" `,
+    function (error, results, fields) {
+      res.send(`
             <h1>${req.params.title}</h1>
             <h3>${results[0].description}</h3>
             
@@ -164,24 +183,25 @@ app.get('/page/:title', (req, res) => {
             <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
             <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
             <style> form {display:flex}, h1 {color:red} </style>
-        `)
-    })
-})
+        `);
+    }
+  );
+});
 
-app.post('/update/:title',(req,res)=>{
-    const title = req.params.title
-    const nickname = req.session.nickname
-    const logined= req.session.logined
-    console.log(req.body)
-    console.log(req.body.description)
+app.post("/update/:title", (req, res) => {
+  const title = req.params.title;
+  const nickname = req.session.nickname;
+  const logined = req.session.logined;
 
-    connection.query(`select writer from writing_list where title = "${title}"`, function (error, results, fields) {
-        const writer = results[0].writer
+  connection.query(
+    `select writer from writing_list where title = "${title}"`,
+    function (error, results, fields) {
+      const writer = results[0].writer;
 
-        if (nickname != writer || logined == false) {
-            res.redirect('/')
-        } else {
-            res.send(`
+      if (nickname != writer || logined == false) {
+        res.redirect("/");
+      } else {
+        res.send(`
             <form action="/update/${title}?_method=PUT" method="POST">
                 <p>
                     <input name="new_title" type="text" value="${title}">
@@ -195,127 +215,152 @@ app.post('/update/:title',(req,res)=>{
                     <input type="submit" class="fadeIn fourth" value="제출 하기">
                 </p>
             </form>
-            `)
-        }
-    })
-})
+            `);
+      }
+    }
+  );
+});
 
-app.put('/update/:title',(req,res)=>{
+app.put("/update/:title", (req, res) => {
+  connection.query(
+    `update writing_list set title="${req.body.new_title}", description="${req.body.new_description}" 
+    where title="${req.body.old_title}" and description="${req.body.old_description}" and writer="${req.session.nickname}"`,
+    function (error, results, fields) {
+      res.redirect(`/`);
+    }
+  );
+});
 
-    connection.query(`update writing_list set title="${req.body.new_title}", description="${req.body.new_description}" 
-    where title="${req.body.old_title}" and description="${req.body.old_description}" and writer="${req.session.nickname}"`, function (error, results, fields) {
-        res.redirect(`/`)        
-    })
-})
+app.delete("/delete/:title", (req, res) => {
+  const title = req.params.title;
+  const nickname = req.session.nickname;
+  const logined = req.session.logined;
+  connection.query(
+    `select writer from writing_list where title = "${title}"`,
+    function (error, results, fields) {
+      const writer = results[0].writer;
 
-app.delete('/delete/:title', (req, res) => {
-    const title = req.params.title
-    const nickname = req.session.nickname
-    const logined= req.session.logined
-    connection.query(`select writer from writing_list where title = "${title}"`, function (error, results, fields) {
-        const writer = results[0].writer
+      if (nickname != writer || logined == false) {
+        res.redirect("/");
+      } else {
+        connection.query(
+          `delete from writing_list where title = "${title}"`,
+          function (error, results, fields) {
+            res.redirect("/");
+          }
+        );
+      }
+    }
+  );
+});
 
-        if (nickname != writer || logined == false) {
-            res.redirect('/')
-        } else {
-            connection.query(`delete from writing_list where title = "${title}"`, function (error, results, fields) {
-                res.redirect('/') 
-            })
-        }
-    })
+app.get("/signin", (req, res) => {
+  // 로그인 창
+  var body = "";
+  fs.readFile("./views/login/signin.html", "utf8", (err, data) => {
+    body = data;
+    res.send(body);
+  });
+});
 
-})
+app.post("/signin", (req, res) => {
+  var id = req.body.id;
+  var pw = req.body.pw;
+  connection.query(
+    `select salt,pw,nickname from user_list where id="${id}"`,
+    function (error, results) {
+      if (results[0] == undefined) {
+        connection.end();
+        res.send("<h1>아이디가 일치하지 않습니다</h1>");
+      } else {
+        var salt = results[0].salt;
+        var crypto_pw = results[0].pw;
+        var nickname = results[0].nickname;
+        crypto.pbkdf2(pw, salt, 100000, 10, "sha512", (err, key) => {
+          if (crypto_pw != key.toString("base64")) {
+            res.send("<h1>비밀번호가 일치하지 않습니다</h1>");
+          } else {
+            req.session.save(() => {
+              req.session.logined = true;
+              req.session.nickname = nickname;
+              res.redirect("/");
+            });
+          }
+        });
+      }
+    }
+  );
+});
 
-app.get('/signin', (req, res) => { // 로그인 창 
-    var body = ""
-    fs.readFile('./views/login/signin.html','utf8',(err,data)=>{
-        body = data
-        res.send(body)
-    })
-})
+app.get("/signup", (req, res) => {
+  // 회원 가입 폼 요청
+  var body = "";
+  fs.readFile("./views/login/signup.html", "utf8", (err, data) => {
+    body = data;
+    res.send(body);
+  });
+});
 
-app.post('/signin', (req, res) => {
-    var id = req.body.id
-    var pw = req.body.pw
-    connection.query(`select salt,pw,nickname from user_list where id="${id}"`,function(error,results){
-        if (results[0] == undefined) {
-            connection.end()
-            res.send('<h1>아이디가 일치하지 않습니다</h1>')
-        } else {
-            var salt = results[0].salt
-            var crypto_pw = results[0].pw
-            var nickname = results[0].nickname
-            crypto.pbkdf2(pw, salt, 100000, 10, 'sha512', (err, key) => {
-                if (crypto_pw != key.toString('base64')){
-                    res.send('<h1>비밀번호가 일치하지 않습니다</h1>')
-                } else {        
-                    req.session.save(()=>{
-                        req.session.logined = true;
-                        req.session.nickname = nickname;
-                        res.redirect('/')
-                    })
+app.post("/signup", (req, res) => {
+  // 회원가입 정보 DB에 저장
+  var body = req.body; // 회원정보 객체 id/ pw / nickname
+  var id = body.id;
+  var pw = body.pw;
+  var salt = "";
+  var nickname = body.nickname;
+
+  connection.query(
+    `select * from user_list where id=${id} or nickname=${nickname}`,
+    function (error, results) {
+      if (results != undefined) {
+        connection.end();
+        res.redirect("/signup_fail");
+      } else {
+        crypto.randomBytes(10, (err, buf) => {
+          crypto.pbkdf2(
+            body.pw,
+            buf.toString("base64"),
+            100000,
+            10,
+            "sha512",
+            (err, key) => {
+              salt = buf.toString("base64");
+              pw = key.toString("base64");
+              connection.query(
+                `INSERT INTO user_list(id, pw, salt, nickname) VALUES ("${id}", "${pw}", "${salt}","${nickname}")`,
+                function (error, results, fields) {
+                  if (error) {
+                    throw error;
+                  } else {
+                    res.redirect("/");
+                  }
                 }
-            });
-        }
-    })
-})
+              );
+            }
+          );
+        });
+      }
+    }
+  );
+});
 
+app.get("/signup_fail", (req, res) => {
+  // 회원 가입 실패
+  fs.readFile("./views/login/signup_fail.html", "utf8", (err, data) => {
+    res.send(data);
+  });
+});
 
-app.get('/signup', (req, res) => { // 회원 가입 폼 요청
-    var body = ""
-    fs.readFile('./views/login/signup.html','utf8',(err,data)=>{
-        body = data
-        res.send(body)
-    })
-})
+app.get("/logout", (req, res) => {
+  // 로그아웃
+  req.session.logined = false;
+  req.session.save(() => {});
+  req.session.destroy(function () {
+    req.session;
+    res.redirect("/");
+  });
+});
 
-app.post('/signup',(req, res) => { // 회원가입 정보 DB에 저장 
-    var body = req.body // 회원정보 객체 id/ pw / nickname 
-    var id = body.id
-    var pw = ""
-    var salt = ""
-    var nickname = body.nickname
+app.listen(port);
 
-    connection.query(`select * from user_list where id=${id} or nickname=${nickname}`,function(error,results){
-        console.log(results)
-        if(results[0] != undefined){
-            console.log(0)
-            connection.end()
-            res.redirect('/signup_fail')
-        } else {
-            console.log(1)
-            crypto.randomBytes(10, (err, buf) => {
-                crypto.pbkdf2(body.pw, buf.toString('base64'), 100000, 10, 'sha512', (err, key) => {
-                  salt = buf.toString('base64')
-                  pw = key.toString('base64');
-                  connection.query(`INSERT INTO user_list(id, pw, salt, nickname) VALUES ("${id}", "${pw}", "${salt}","${nickname}")`, function (error, results, fields) {
-                    if (error) {
-                        throw(error)
-                    } else {
-                        res.redirect("/")
-                    };
-                  });
-                });
-            });
-        }
-    })
-    
-})
-
-app.get('/signup_fail',(req, res)=>{ // 회원 가입 실패
-    fs.readFile('./views/login/signup_fail.html','utf8',(err,data)=>{
-        res.send(data)
-    })
-})
-
-app.get('/logout',(req,res)=>{ // 로그아웃 
-    req.session.logined = false 
-    req.session.save(()=>{
-    })
-    req.session.destroy(function(){
-        req.session;
-        res.redirect('/');
-    });
-})
-
-app.listen(port)
+console.log("Start!");
